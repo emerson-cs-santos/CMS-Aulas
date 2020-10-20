08db69ba0f5b4bfa223d617b0d6f942103aa5e95
Emerson Costa <syphonemerson@yahoo.com.br>
2020-10-19 22:34:01 -0300

    Aula 19-10-2020

--- wordpress/wp-content/plugins/plugin_ola_mundo/plugin_ola_mundo.php
+++ wordpress/wp-content/plugins/plugin_ola_mundo/plugin_ola_mundo.php
@@ -12,9 +12,35 @@
 // Exemplo usando filter hook - Inicio
 add_filter( 'login_errors', 'nova_mensagem_de_erro');
 
-function nova_mensagem_de_erro()
+function nova_mensagem_de_erro(): string
 {
     return 'Credenciais inválidas!';
 }
 // Exemplo usando filter hook - Fim
 
+
+
+// Exemplo usando action hook - Inicio
+add_action('wp_head', 'colocaComentario');
+
+function colocaComentario()
+{
+    // is_single = se retornar true, significa que o usuário está em uma página de um unico post
+    if ( is_single() ) 
+    {
+      //  echo "<!-- Comentário de exemplo -->";
+      // get_the_title - Pega titulo do post
+      echo '\\n \\n';
+
+      echo '<meta property="og:title" content="' . get_the_title() .'">';
+
+      $title = wp_title('', false);
+      //$title = get_site();
+      echo '<meta property="og:site_name" content="' . $title .'">';
+
+      echo '<meta property="og:url" content="' . get_permalink( get_the_ID() ) .'">';
+
+      echo '\\n \\n';
+    }
+}
+// Exemplo usando action hook - Fim
